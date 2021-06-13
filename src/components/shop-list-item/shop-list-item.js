456a8a32a3f9110465ae0b057fa8ce0750b3bd19
Ivan <bomak54@gmail.com>
2021-06-13 15:03:44 +0300

    add style

--- src/components/shop-list-item/shop-list-item.js
+++ src/components/shop-list-item/shop-list-item.js
@@ -1,16 +1,16 @@
 import React from "react";
-
+import './shop-list-item.sass'
 const ShopListItem = ({item}) => {
     const {product, category, price} = item.items
     return (
-        <div>
-            <div>
+        <div className='shop-list-item'>
+            <div className='item'>
                 {product}
             </div>
-            <div>
+            <div className='item'>
                 {category}
             </div>
-            <div>
+            <div className='item'>
                 {price}
             </div>
         </div>
--- src/components/shop-list/shop-list.js
+++ src/components/shop-list/shop-list.js
@@ -1,23 +1,32 @@
 import React from "react";
 import ShopListItem from "../shop-list-item";
+import './shop-list.sass'
 
 const ShopList = ({items}) => {
     return (
-        <div>
-           <span>Product</span>
-           <span>Category</span>
-           <span>Price</span>
-            <ul>
-                {
-                    items.map((item, index) => {
-                        return (
-                            <li key={index + 1}>
-                                <ShopListItem item={item} />
-                            </li>
-                        )
-                    })
-                }
-            </ul>
+        <div className='shop-list-container'>
+            <div className='shop-list'>
+                <div className='shop-list-title'>
+                    <span>Product</span>
+                    <span>Category</span>
+                    <span>Price</span>
+                </div>
+
+                <ul>
+                    {
+                        items.map((item, index) => {
+                            return (
+                                <li key={index + 1}>
+                                    <ShopListItem item={item}/>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+                <div className='border'>
+
+                </div>
+            </div>
         </div>
     );
 };
